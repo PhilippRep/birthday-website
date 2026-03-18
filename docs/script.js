a16ded7df5593a312f97c31a787a7fc5dae15a7a
Philipp <philipp_repke@gmx.de>
2026-03-18 17:47:40 +0100

    new image postions

--- docs/script.js
+++ docs/script.js
@@ -3,9 +3,9 @@
 // =======================
 
 const startImages = [
-    { src: "DSCF5081.jpg", style: { top:"10vh", left:"50%", transform:"translateX(-50%)" } }, // oben mittig
-    { src: "DSCF3972.jpg", style: { bottom:"3px", left:"10%" } },                             // unten links
-    { src: "DSCF2395.jpg", style: { bottom:"3px", right:"10%" } }                             // unten rechts
+    { src: "DSCF5081.jpg", style: { top:"3vh", left:"50%", transform:"translateX(-50%)" } }, // oben mittig
+    { src: "DSCF3972.jpg", style: { bottom:"1px", left:"10%" } },                             // unten links
+    { src: "DSCF2395.jpg", style: { bottom:"1px", right:"10%" } }                             // unten rechts
 ];
 
 
